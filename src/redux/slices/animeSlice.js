12c0addc0a4bd449a5af810506d62baf20d46f43
elmira1 <mirzaee@kth.se>
2025-12-03 12:11:00 +0100

    update animeSlice - userSlice
    vill fortsätta under veckan

--- src/redux/slices/animeSlice.js
+++ src/redux/slices/animeSlice.js
@@ -15,9 +15,71 @@
  ***********************************************************************/
 
 // TODO:
-// 1. import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// 1.
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
 // 2. Import animeSource functions: searchAnime, getTopAnime, getGenres
-// 3. initialState = {
+import { searchAnime, getTopAnime, getGenres } from '../../sources/animeSource';
+import { resolvePromise } from '../../utils/reduxUtils';
+
+// 3. Define initialState:
+/************* 1. Hjälpfunktion: tomt promiseState *************/
+function makePromiseState() {
+    return {
+        promise: null,
+        data: null,
+        error: null
+    };
+}
+
+/************* 2. initialState *************/
+const initialState = {
+    search:   { promiseState: makePromiseState() },
+    trending: { promiseState: makePromiseState() },
+    genres:   { promiseState: makePromiseState() }
+};
+
+
+// ************************4. Create thunks:**********
+// Sök anime baserat på namn (query)
+export const fetchSearch = createAsyncThunk(
+    'anime/fetchSearch',    
+    async (query) => {
+        // query kan vara tom / null, hantera det uppåt i presenter vid behov
+        const data = await searchAnime(query);
+        return data;// blir action.payload i fulfilled
+    }   
+);
+
+//fetchTrending()
+// Hämta top / trending anime
+export const fetchTrending = createAsyncThunk(
+    'anime/fetchTrending',    
+    async () => {
+        const data = await getTopAnime();
+        return data;
+    }   
+);
+
+//fetchGenres()
+// Hämta genre-lista
+export const fetchGenres = createAsyncThunk(
+    'anime/fetchGenres',    
+    async () => {   
+        const data = await getGenres();
+        return data;
+    }
+);
+
+
+//************************ */ 5. createSlice({*****************
+
+
+        
+
+
+
+//initialState = {
 //       search: { promiseState: { promise: null, data: null, error: null }},
 //       trending: { promiseState: {} },
 //       genres: { promiseState: {} }
