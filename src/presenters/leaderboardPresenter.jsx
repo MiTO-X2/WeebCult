646430e17b9b8696e88e22aeba288e8700c5d9ec
Andreas <andana@kth.se>
2025-12-10 16:37:41 +0100

    Create leaderboardPresenter.jsx

---
+++ src/presenters/leaderboardPresenter.jsx
@@ -0,0 +1,59 @@
+/***********************************************************************
+ * PURPOSE:
+ *   - Present leaderboard data in a modal popup
+ *   - Presenter = Redux-connected container for LeaderboardView
+ *   - Fetch leaderboard entries and user's own entry
+ *   - Pass all necessary props + callbacks to the view
+ *   - Handle suspense / loading states
+ *   - Handle modal open/close
+ ***********************************************************************/
+
+import { connect } from "react-redux";
+import { LeaderboardView } from "../views/leaderboardView.jsx";
+// TODO: import leaderboard thunks from leaderboardSlice
+import { SuspenseView } from "../views/suspenseView.jsx";
+
+// ------------------- mapStateToProps -------------------
+function mapStateToProps(state) {
+    return {
+        // TODO: leaderboard entries array
+        leaderboardEntries: state.leaderboard.entries,
+        // TODO: user's own leaderboard entry
+        userEntry: state.leaderboard.userEntry,
+        // TODO: modal open state
+        isOpen: state.leaderboard.isOpen,
+        // TODO: loading state / pending promises
+        isLoading: state.leaderboard.loading
+    };
+}
+
+// ------------------- mapDispatchToProps -------------------
+const mapDispatchToProps = {
+    // TODO: close leaderboard modal
+    onClose: () => (""),
+    // TODO: fetch leaderboard entries
+    fetchLeaderboard: () => (""),
+    // TODO: fetch user's own leaderboard entry
+    fetchUserEntry: () => ("")
+};
+
+// ------------------- Presenter Component -------------------
+function LeaderboardPresenterComponent(props) {
+    if (!props.isOpen) return null; // modal closed
+
+    // TODO: show loader if data is loading
+    if (props.isLoading) return <SuspenseView />;
+
+    // TODO: fetch leaderboard data if not already loaded
+
+    return (
+        <LeaderboardView
+            {...props}
+            // props include leaderboardEntries, userEntry, onClose
+        />
+    );
+}
+
+// ------------------- Connect -------------------
+export const LeaderboardPresenter =
+    connect(mapStateToProps, mapDispatchToProps)(LeaderboardPresenterComponent);
