81f8536096aa983c51cdbb455dd341c991a52a89
Andreas <andana@kth.se>
2025-12-16 11:56:22 +0100

    Update leaderboardPresenter.jsx

--- src/presenters/leaderboardPresenter.jsx
+++ src/presenters/leaderboardPresenter.jsx
@@ -11,45 +11,33 @@
 import { connect } from "react-redux";
 import { LeaderboardView } from "../views/leaderboardView.jsx";
 import { SuspenseView } from "../views/suspenseView.jsx";
-import { fetchLeaderboard, fetchUserLeaderboardEntry } from "../redux/slices/leaderboardSlice.js";
+import { closeLeaderboard } from "../redux/slices/leaderboardSlice.js";
 
 // ------------------- mapStateToProps -------------------
 function mapStateToProps(state) {
     return {
-        // TODO: leaderboard entries array
-        leaderboardEntries: state.leaderboard.entries,
-        // TODO: user's own leaderboard entry
+        leaderboardEntries: state.leaderboard.entries || [],
         userEntry: state.leaderboard.userEntry,
-        // TODO: modal open state
         isOpen: state.leaderboard.isOpen,
-        // TODO: loading state / pending promises
-        isLoading: state.leaderboard.loading
+        isLoading: state.leaderboard.loading,
+        userUid: state.user.uid
     };
 }
 
 // ------------------- mapDispatchToProps -------------------
 const mapDispatchToProps = {
-    // TODO: close leaderboard modal
-    // onClose: closeLeaderboard,
-    // TODO: fetch leaderboard entries
-    onfetchLeaderboard: fetchLeaderboard,
-    // TODO: fetch user's own leaderboard entry
-    fetchUserEntry: fetchUserLeaderboardEntry
+    onClose: closeLeaderboard
 };
 
 // ------------------- Presenter Component -------------------
 function LeaderboardPresenterComponent(props) {
     if (!props.isOpen) return null; // modal closed
 
-    // TODO: show loader if data is loading
     if (props.isLoading) return <SuspenseView />;
-
-    // TODO: fetch leaderboard data if not already loaded
 
     return (
         <LeaderboardView
             {...props}
-            // props include leaderboardEntries, userEntry, onClose
         />
     );
 }
