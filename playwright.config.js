251c5debe155e539f2b7d2e043749674420e5849
Derfesh Mariush <derfesh@kth.se>
2026-09-09 18:51:00 +0200

    Plz github actions work

--- playwright.config.js
+++ playwright.config.js
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run preview -- --port 8080',
     url: 'http://localhost:8080',
     reuseExistingServer: !process.env.CI,
   },
