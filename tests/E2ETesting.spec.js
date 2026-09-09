1cf59c8486fb81a6b15f6384e0eb35c7b1371285
Derfesh Mariush <derfesh@kth.se>
2026-09-09 18:10:47 +0200

    small changes

--- tests/E2ETesting.spec.js
+++ tests/E2ETesting.spec.js
@@ -5,5 +5,14 @@ import { test, expect } from '@playwright/test';
 test('Check for example a trending anime exists', async ({ page }) => {
   await page.goto('http://localhost:8080/');
   await expect(page.getByText('Sousou no FrierenTV2023PG-')).toBeVisible();
-  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  await page.screenshot({ path: './test-results/Trending.png', fullPage: true });
 });
+
+test('Check if leader works fine', async ({ page }) => {
+  await page.goto('http://localhost:8080/');
+  await page.getByRole('button', { name: ' Open the global leaderboard' }).click();
+  await expect(page.getByRole('heading', { name: 'Leaderboard' })).toBeVisible();
+  await page.getByText('Leaderboard✕#UserTotal').click();
+  await page.screenshot({ path: './test-results/Leaderboard.png', fullPage: false });
+});
+
